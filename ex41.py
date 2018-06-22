c6eab2bf9c6d25adf18203094209c0ee58a9b7ef
ArthurMinSein <arthurmin93@gmail.com>
2018-06-22 13:53:32 +0630

    add sitepackage.py

--- ex41.py
+++ ex41.py
@@ -39,17 +39,17 @@ def convert(snippet, phrase):
     for sentence in snippet, phrase:
         result = sentence[:]
 # fake class names
-    for word in class_names:
-        result = result.replace("%%%", word, 1)
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
 
 # fake other names
-    for word in other_names:
-        result = result.replace("***", word, 1)
+        for word in other_names:
+            result = result.replace("***", word, 1)
 # fake parameter lists
-    for word in param_names:
-        result = result.replace("@@@", word, 1)
+        for word in param_names:
+            result = result.replace("@@@", word, 1)
 
-    results.append(result)
+        results.append(result)
     return results
 # keep going until they hit CTRL-D
 try:
